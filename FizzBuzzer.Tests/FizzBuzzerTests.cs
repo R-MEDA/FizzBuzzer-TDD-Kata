dc29010ec97a3d47bb8a7aa3d91f72c11a425783
Rashid Meda <20155727@student.hhs.nl>
2025-05-20 00:42:33 +0200

    refactor FizzBuzz testsuite

--- FizzBuzzer.Tests/FizzBuzzerTests.cs
+++ FizzBuzzer.Tests/FizzBuzzerTests.cs
@@ -1,59 +1,60 @@
-﻿namespace FizzBuzzer.Tests;
-
-public class FizzBuzzerTest
+﻿namespace FizzBuzzer.Tests
 {
-    [Theory]
-    [InlineData(3)]
-    [InlineData(6)]
-    [InlineData(9)]
-    [InlineData(12)]
-    public void GivenNumberIsMultipleOfThree_WhenFizzBuzzerRuns_ThenReturnsFizz(int number)
+    public class FizzBuzzerTests
     {
-        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+        private readonly FizzBuzzer fizzBuzzer;
 
-        string result = fizzBuzzer.FizzBuzz(number);
+        public FizzBuzzerTests()
+        {
+            fizzBuzzer = new FizzBuzzer();
+        }
 
-        Assert.Equal("Fizz", result);
-    }
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(12)]
+        public void GivenNumberIsMultipleOfThree_WhenFizzBuzzerRuns_ThenReturnsFizz(int number)
+        {
+            string result = fizzBuzzer.FizzBuzz(number);
 
-    [Theory]
-    [InlineData(5)]
-    [InlineData(10)]
-    [InlineData(20)]
-    public void GivenNumberIsMultipleOfFive_WhenFizzBuzzerRuns_ThenReturnsBuzz(int number)
-    {
-        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+            Assert.Equal("Fizz", result);
+        }
 
-        string result = fizzBuzzer.FizzBuzz(number);
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void GivenNumberIsMultipleOfFive_WhenFizzBuzzerRuns_ThenReturnsBuzz(int number)
+        {
+            string result = fizzBuzzer.FizzBuzz(number);
 
-        Assert.Equal("Buzz", result);
-    }
+            Assert.Equal("Buzz", result);
+        }
 
-    [Theory]
-    [InlineData(15)]
-    [InlineData(30)]
-    [InlineData(45)]
-    [InlineData(90)]
-    public void GivenNumberIsMultipleOfBothThreeAndFive_WhenFizzBuzzerRuns_ThenReturnsFizzBuzz(int number)
-    {
-        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        [InlineData(90)]
+        public void GivenNumberIsMultipleOfBothThreeAndFive_WhenFizzBuzzerRuns_ThenReturnsFizzBuzz(int number)
+        {
 
-        string result = fizzBuzzer.FizzBuzz(number);
+            string result = fizzBuzzer.FizzBuzz(number);
 
-        Assert.Equal("FizzBuzz", result);
-    }
+            Assert.Equal("FizzBuzz", result);
+        }
 
-    [Theory]
-    [InlineData(1)]
-    [InlineData(2)]
-    [InlineData(4)]
-    [InlineData(7)]
-    public void GivenNumberIsNeitherMultipleOThreeNorFive_WhenFizzBuzzerRuns_ThenReturnsFizzBuzz(int number)
-    {
-        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [InlineData(7)]
+        public void GivenNumberIsNeitherMultipleOThreeNorFive_WhenFizzBuzzerRuns_ThenReturnsFizzBuzz(int number)
+        {
+            string result = fizzBuzzer.FizzBuzz(number);
 
-        string result = fizzBuzzer.FizzBuzz(number);
-
-        Assert.Equal(number.ToString(), result);
+            Assert.Equal(number.ToString(), result);
+        }
     }
 }
