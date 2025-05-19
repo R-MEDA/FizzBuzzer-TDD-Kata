0f8f763ef011dac7dc634206e6f2866744639645
Rashid Meda <20155727@student.hhs.nl>
2025-05-20 00:27:02 +0200

    write test for the second specification. Numbers divisible by 5 should return "buzz".

--- FizzBuzzer.Tests/FizzBuzzerTests.cs
+++ FizzBuzzer.Tests/FizzBuzzerTests.cs
@@ -15,4 +15,18 @@ public class FizzBuzzerTest
 
         Assert.Equal("Fizz", result);
     }
+
+    [Theory]
+    [InlineData(5)]
+    [InlineData(10)]
+    [InlineData(15)]
+    [InlineData(20)]
+    public void GivenNumberIsMultipleOfFive_WhenFizzBuzzerRuns_ThenReturnsBuzz(int number)
+    {
+        FizzBuzzer fizzBuzzer = new FizzBuzzer();
+
+        string result = fizzBuzzer.FizzBuzz(number);
+
+        Assert.Equal("Buzz", result);
+    }
 }
